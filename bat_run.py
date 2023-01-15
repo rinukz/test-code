3a2398fd8dbc35372f5827d363af2c13e21bbca3
Rinukz <98143840+rinukz@users.noreply.github.com>
2023-01-15 21:40:36 +0700

    autoCommit 2023 0Su.214036

--- bat_run.py
+++ bat_run.py
@@ -1,3 +1,6 @@
 import subprocess
+import time as tt
+while True:
+    subprocess.call(["C:/Users/Rinukz02/Desktop/github_auto_snyc.bat"])
 
-subprocess.call(["C:/Users/Rinukz02/Desktop/github_auto_snyc.bat"])
+    tt.sleep(3)
